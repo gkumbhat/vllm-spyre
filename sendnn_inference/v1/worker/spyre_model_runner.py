:bug: Enable inference mode for MM encoding computation
Signed-off-by: Gaurav-Kumbhat <Gaurav.Kumbhat@ibm.com>

sendnn_inference/v1/worker/spyre_model_runner.py
@@ -1041,11 +1041,12 @@ class ChunkedPrefillModelRunner(
             ).unsqueeze(0)
 
             t0 = time.time()
-            full_embeds = self.model.get_maybe_mm_embeddings(
-                full_input_tokens,
-                mm_features=mm_features,
-                is_decode=False,
-            )
+            with torch.inference_mode():
+                full_embeds = self.model.get_maybe_mm_embeddings(
+                    full_input_tokens,
+                    mm_features=mm_features,
+                    is_decode=False,
+                )
 
             t_elapsed = time.time() - t0
 