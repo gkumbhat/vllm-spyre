7def4ff84f1737cb2e90256909516d6bae086e17
gkumbhat <Gaurav.Kumbhat@ibm.com>
2026-06-12 13:18:33 -0500

    :art: Fix ty checks
    Signed-off-by: gkumbhat <Gaurav.Kumbhat@ibm.com>

--- sendnn_inference/v1/worker/spyre_model_runner.py
+++ sendnn_inference/v1/worker/spyre_model_runner.py
@@ -362,7 +362,7 @@ class SpyrePoolingModelRunner(
 
         if task == "classify":
             tokenizer = AutoTokenizer.from_pretrained(self.model_config.model)
-            output = tokenizer(text="foo", text_pair="bar")
+            output = tokenizer(text="foo", text_pair="bar")  # ty: ignore[call-non-callable]
             self.use_token_type_ids = "token_type_ids" in output
             if self.use_token_type_ids:
                 self.sep_token_id = tokenizer.sep_token_id
